3467f7d502f9569ab63d59b93b5180a376920257
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-09-20 22:51:23 -0300

    adicionado temporizador para a duração de visualização de barra de progresso

--- app/src/main/java/br/com/brunoloures/lmsapp/telaInicialActivity.kt
+++ app/src/main/java/br/com/brunoloures/lmsapp/telaInicialActivity.kt
@@ -3,11 +3,11 @@ package br.com.brunoloures.lmsapp
 import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -26,9 +26,9 @@ class telaInicialActivity : DebugActivity  () {
 
         progressBar.visibility = View.GONE
 
-        var btnPedidos = findViewById<Button>(R.id.botao_pedidos);
-        var btnNovoPedido = findViewById<Button>(R.id.botao_novo_pedido);
-        var btnPerfil = findViewById<Button>(R.id.botao_perfil);
+        var btnPedidos = findViewById<Button>(R.id.botao_pedidos)
+        var btnNovoPedido = findViewById<Button>(R.id.botao_novo_pedido)
+        var btnPerfil = findViewById<Button>(R.id.botao_perfil)
 
         btnPedidos.setOnClickListener(View.OnClickListener {
             Toast.makeText(this, "Pedidos", Toast.LENGTH_SHORT).show()
@@ -68,10 +68,20 @@ class telaInicialActivity : DebugActivity  () {
             ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
                 .setDuration(1000)
                 .start()
+
+            val timer = object: CountDownTimer(10000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {}
+
+                override fun onFinish() {
+                    progressBar.visibility = View.GONE;
+                }
+            }
+
+            timer.start()
+
             if(progressBar.max < 100){
                 progressBar.visibility = View.GONE
             }
-
         } else if (id == R.id.action_config) {
             Toast.makeText(this, "Configuração", Toast.LENGTH_SHORT).show()
             var intent = Intent(this, Config::class.java)
